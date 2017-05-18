ec0a0d26242cde3ee006e62416d9f4845519514c
Fabien Turgut <Fabien Turgut>
2017-05-18 15:12:11 +0200

    add angular js

---
+++ public/js/myAngular.js
@@ -0,0 +1,11 @@
+(function() {
+  var app = angular.module('users', []);
+
+  app.controller('ReviewController', function(){
+    this.review = {
+      firstname: "fabien",
+      lastname: "turgut"
+    }
+  });
+
+});
