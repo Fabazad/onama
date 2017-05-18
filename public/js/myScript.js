c9738bbe8aee3dfeb8786d2effd7f3156055b732
Fabien Turgut <Fabien Turgut>
2017-05-18 21:00:55 +0200

    remise nav

--- public/js/myScript.js
+++ public/js/myScript.js
@@ -3,4 +3,7 @@ $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 200 // Creates a dropdown of 15 years to control year
   });
-$('#nav-mobile').click($('.button-collapse').sideNav('hide'));
+$('#nav-mobile').click(function(){
+  $('.button-collapse').sideNav('hide');
+  alert("test");
+});
