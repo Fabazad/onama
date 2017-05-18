2bc515b39153d8398ae68b190648a91f8dcc2dc8
Fabien Turgut <Fabien Turgut>
2017-05-18 23:00:02 +0200

    test pg

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var user = require("node_modules/user");
+var user = require("user");
 //var favicon = require('serve-favicon');
 
 app.set('port', (process.env.PORT || 5000));
