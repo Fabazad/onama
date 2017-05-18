7ef32fe8effd35674a67ef9b705d8812a88a74f0
Fabien Turgut <Fabien Turgut>
2017-05-18 23:15:31 +0200

    test pg

--- public/js/myAngular.js
+++ public/js/myAngular.js
@@ -1,18 +1,18 @@
 (function() {
   var app = angular.module('app', []);
 
-    app.controller("ConnectionCtrl", ["$http", function($http){
+    app.controller("ConnectionCtrl", function($http){
       this.connection = {};
       this.getConnection = function(){
         alert(this.connection.password);
 
-        $http.put('/connection', { param: this.connection }).success(function(data){
+        /*$http.put('/connection', { param: this.connection }).success(function(data){
           alert(data);
-        });
+        });*/
 
         this.connection = {};
       };
-    }]);
+    });
 
     app.controller("InscriptionCtrl", function(){
       this.inscription = {};
