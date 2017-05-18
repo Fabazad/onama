55066aecce81f115d6bb19a8613f9d8fb312d2f8
Fabien Turgut <Fabien Turgut>
2017-05-18 23:07:52 +0200

    test pg

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function(request, response) {
 })
 .get('/connection', function (req, res) {
   res.writeHead(200, {"Content-Type": "text/html"});
-  res.end(user.lire());
+  res.end("hey");
 });
 
 app.listen(app.get('port'), function() {
