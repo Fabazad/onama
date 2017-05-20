5652880faf178d53864b05904a0e5c3f3619c003
Fabien Turgut <Fabien Turgut>
2017-05-20 23:39:13 +0200

    ajout chargement

--- public/js/myAngular.js
+++ public/js/myAngular.js
@@ -37,82 +37,78 @@
 
 /*Controllers*/
 
-app.controller("UserCtrl", ["$http", "$cookies", function($http, $cookies){
-  this.connection = {};
-  this.inscription = {};
-  var userCtrl = this;
-
-  var cookie = {cookiecode : $cookies.get('cookiecode')};
-  if(cookie.cookiecode){
-    this.chargement = true;
-    $http.post('/connectionCookie', cookie)
-    .then(function(response){
-      if("error" in response.data){
-        alert(response.data.error);
-      }
-      else {
-        userCtrl.user = response.data;
-      }
-      this.chargement = false;
-    });
-  }
-
-  this.connected = function(){
-    return("user" in this);
-  }
-
-  this.deconnection = function(){
-    delete this.user;
+  app.controller("UserCtrl", ["$http", "$cookies", function($http, $cookies){
     this.connection = {};
     this.inscription = {};
-    $cookies.remove("cookiecode");
-  }
+    var userCtrl = this;
 
-  this.getConnection = function(){
-    $http.post('/connection', this.connection).then(function(response){
-      if("error" in response.data){
-        alert(response.data.error);
-      }
-      else {
-        userCtrl.user = response.data;
-        if(response.data.stayconnected){
-          var expireDate = new Date();
-          expireDate.setDate(expireDate.getDay() - 7);
-          $cookies.put('cookiecode', response.data.cookiecode, {"expires" : expireDate} );
+    var cookie = {cookiecode : $cookies.get('cookiecode')};
+    if(cookie.cookiecode){
+      this.chargement = true;
+      $http.post('/connectionCookie', cookie)
+      .then(function(response){
+        if("error" in response.data){
+          alert(response.data.error);
         }
         else {
+          userCtrl.user = response.data;
+        }
+        this.chargement = false;
+      });
+    }
+
+    this.connected = function(){
+      return("user" in this);
+    }
+
+    this.deconnection = function(){
+      delete this.user;
+      this.connection = {};
+      this.inscription = {};
+      $cookies.remove("cookiecode");
+    }
+
+    this.getConnection = function(){
+      this.chargement = true;
+      $http.post('/connection', this.connection).then(function(response){
+        if("error" in response.data){
+          alert(response.data.error);
+        }
+        else {
+          userCtrl.user = response.data;
+          if(response.data.stayconnected){
+            var expireDate = new Date();
+            expireDate.setDate(expireDate.getDay() - 7);
+            $cookies.put('cookiecode', response.data.cookiecode, {"expires" : expireDate} );
+          }
+          else {
+            var expireDate = new Date();
+            expireDate.setMinutes(expireDate.getMinutes() + 20);
+            $cookies.put('cookiecode', response.data.cookiecode, {"expires" : expireDate} );
+          }
+        }
+        this.chargement = false;
+        this.connection = {};
+      });
+    };
+
+
+    this.getInscription = function(inscription){
+      this.chargement = true;
+      $http.post('/inscription', this.inscription).then(function(response){
+        if("error" in response.data){
+          alert(response.data.error);
+        }
+        else {
+          userCtrl.user = response.data;
           var expireDate = new Date();
           expireDate.setMinutes(expireDate.getMinutes() + 20);
-          $cookies.put('cookiecode', response.data.cookiecode, {"expires" : expireDate} );
+          $cookies.put('cookiecode', response.data.cookiecode, {"expires" : expireDate });
         }
-      }
-      this.connection = {};
-    });
-  };
-
-
-  this.getInscription = function(inscription){
-    $http.post('/inscription', this.inscription).then(function(response){
-      if("error" in response.data){
-        alert(response.data.error);
-      }
-      else {
-        userCtrl.user = response.data;
-        var expireDate = new Date();
-        expireDate.setMinutes(expireDate.getMinutes() + 20);
-        $cookies.put('cookiecode', response.data.cookiecode, {"expires" : expireDate });
-      }
-      this.inscription = {};
-    });
-  };
-}]);
-
-
-app.directive('nav-onama', function(){
-  return {
-    restrict: 'E',
-    templateUrl: 'http://localhost:5000/partials'
-  };
-});
+        this.chargement = false;
+        this.inscription = {};
+      });
+    };
+  }]);
 
 })();
